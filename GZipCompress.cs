fa025cde4111ebd857b8d5210311b939a7678138
Admiral H. Curtiss <pikachu025@gmail.com>
2020-01-30 23:01:59 +0100

    Move LastRanker code from https://github.com/AdmiralCurtiss/HyoutaTools

---
+++ GZipCompress.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastRankerTools {
+	public class GZipCompress : HyoutaPluginBase.IProgram {
+		public static int ExecuteCompress(List<string> args) {
+			if (args.Count < 2) {
+				Console.WriteLine("Usage: infile outfile");
+				return -1;
+			}
+
+			Compress(new FileStream(args[0], FileMode.Open), args[1]);
+
+			return 0;
+		}
+
+		public static void Compress(FileStream fs, string outpath) {
+			fs.Position = 0;
+			var outfile = new FileStream(outpath, System.IO.FileMode.Create);
+			using (var compressed = new Ionic.Zlib.GZipStream(outfile, Ionic.Zlib.CompressionMode.Compress)) {
+				byte[] buffer = new byte[4096];
+				int numRead;
+				while ((numRead = fs.Read(buffer, 0, buffer.Length)) != 0) {
+					compressed.Write(buffer, 0, numRead);
+				}
+			}
+			outfile.Close();
+		}
+
+		public int Execute(List<string> args) {
+			return ExecuteCompress(args);
+		}
+
+		public List<string> Identifiers() {
+			return new List<string>() { "Other.GZip.Compress" };
+		}
+	}
+}
---
+++ GraceNote/StringExport.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using HyoutaTools.LastRanker;
+
+namespace HyoutaTools.GraceNote.LastRanker {
+	public class StringExport : HyoutaPluginBase.IProgram {
+		public static int Export( List<string> args ) {
+			if ( args.Count < 2 ) {
+				Console.WriteLine( "Usage: db outfile" );
+				return -1;
+			}
+
+			string InDatabase = args[0];
+			string OutFile = args[1];
+			StringFile StringFile = new StringFile();
+			ReadDatabase( StringFile, "Data Source=" + InDatabase );
+			StringFile.CreateFile( OutFile );
+
+			return 0;
+		}
+
+		public static void ReadDatabase( StringFile StringFile, string ConnectionString ) {
+			SQLiteConnection Connection = new SQLiteConnection( ConnectionString );
+			Connection.Open();
+
+			StringFile.Strings = new List<bscrString>();
+
+			using ( SQLiteTransaction Transaction = Connection.BeginTransaction() )
+			using ( SQLiteCommand Command = new SQLiteCommand( Connection ) ) {
+				Command.CommandText = "SELECT english, PointerRef FROM Text ORDER BY PointerRef";
+				SQLiteDataReader r = Command.ExecuteReader();
+				while ( r.Read() ) {
+					String SQLText;
+
+					try {
+						SQLText = r.GetString( 0 ).Replace( "''", "'" );
+					} catch ( System.InvalidCastException ) {
+						SQLText = "";
+					}
+
+					int PointerRef = r.GetInt32( 1 );
+
+
+					bscrString b = new bscrString();
+					b.Position = (uint)PointerRef;
+					b.String = SQLText;
+					StringFile.Strings.Add( b );
+				}
+
+				Transaction.Rollback();
+			}
+			return;
+		}
+
+		public int Execute(List<string> args) {
+			return Export(args);
+		}
+
+		public List<string> Identifiers() {
+			return new List<string>() { "GraceNote.LastRanker.StringExport" };
+		}
+	}
+}
